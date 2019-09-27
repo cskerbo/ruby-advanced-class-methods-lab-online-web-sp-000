0dc62610a933b31761f55c021ebf6332548967fc
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-27 17:38:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if find_by_name(name) != false
+    if name == find_by_name(name)
       find_by_name(name)
     else
       create_by_name(name)
